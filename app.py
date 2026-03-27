a9d287b1ae6694790064d5cbd1cfd1d23f7ebab7
ivancolantuono <ivanc.colantuono@gmail.com>
2026-03-27 19:42:06 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -445,9 +445,9 @@ elif menu == "🚄 Manutenzione":
                     colore = "🟢"
                 # 🎨 STATO VISIVO (NUOVO)
                 if not record:
-                    stato_html = "<span class="pallino rosso"></span> ATTIVITA' DA ESEGUIRE"
+                    stato_html = '<span class="pallino rosso"></span> ATTIVITA\' DA ESEGUIRE'
                 elif record.get("stato") == "APERTO":
-                    stato_html = "<span class="pallino giallo"></span> ATTIVITA' IN CORSO"
+                    stato_html = '<span class="pallino giallo"></span> ATTIVITA\' IN CORSO'
                 else:
                     stato_html = "<span class="pallino verde"></span> ATTIVITA'ESEGUITA"
     
