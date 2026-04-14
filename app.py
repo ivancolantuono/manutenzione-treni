55a8dd39aaf6a44b362cefa2c491b85f8c30d14b
ivancolantuono <ivanc.colantuono@gmail.com>
2026-04-14 07:57:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1353,7 +1353,7 @@ elif menu == "📌 Open Item":
     # 🟡 VALUTAZIONE
     # ============================
     
-    st.subheader("🟡 In Valutazione")
+    st.subheader("🟡 Monitoraggio")
     
     for item in valutazione:
     
