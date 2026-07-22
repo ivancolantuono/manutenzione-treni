417e1a4c6e27231f55500a92eb67c2e6b0f5b4ae
ivancolantuono <ivanc.colantuono@gmail.com>
2026-07-22 19:13:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ class UrlCellRenderer {
 
 URL_PIS = "https://nlsezrwjvhxvsbycxlxd.supabase.co/storage/v1/object/public/software/Versioni%20PIS.xlsx"
 
-@st.cache_data(ttl=300)
+
 def carica_pis():
 
     df = pd.read_excel(URL_PIS)
